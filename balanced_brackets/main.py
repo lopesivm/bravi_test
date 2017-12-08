31549203c7249451c31167dd3723c129beaa94f3
Ivan Lopes <ivan.lopes@pearson.com>
2017-12-08 17:29:35 -0200

    - Implementation of validate_bracket_input method to comply with existing tests

--- balanced_brackets/main.py
+++ balanced_brackets/main.py
@@ -1,2 +1,21 @@
-def validate_bracket_input(input):
-    return True
+import re
+
+def validate_bracket_input(brack_input):
+    result = True
+    if len(brack_input) % 2:
+        result = False
+    elif not re.match('[\[\]{}\(\)]*$', brack_input):
+        result = False
+    else:
+        matching_list = []
+        matching_pattern = {
+            '}': '{',
+            ')': '(',
+            ']': '['
+        }
+        for char in brack_input:
+            if char in matching_pattern.values():
+                matching_list.append(char)
+            elif matching_list.pop() != matching_pattern[char]:
+                result = False
+    return result
